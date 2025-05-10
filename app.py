918e7ada8e17e2d89f1a669db5e4754d238da246
Matteo1234595 <matteo.dicurzio02@gmail.com>
2025-05-10 22:17:57 +0200

    Delete app.py

--- app.py
+++
@@ -1,6 +0,0 @@
-
-# MINV Pro App
-import streamlit as st
-st.set_page_config(page_title="MINV Pro")
-st.title("MINV Pro App")
-st.write("Questa è una versione iniziale. Caricare qui il file app.py aggiornato.")
